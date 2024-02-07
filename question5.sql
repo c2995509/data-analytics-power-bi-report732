8fabbecefe11ab7e8d73c9152e21356dbd416a3d
c2995509 <alanwatkins159@btinternet.com>
2024-02-07 09:40:40 +0000

    Add files via upload
    Added question 5 SQL & CSV files

---
+++ question5.sql
@@ -0,0 +1,8 @@
+SELECT S.store_type,S.country,D.month_name,ROUND(CAST(sum(O.total_orders*P.sale_price) as BIGINT)) as Monthly_Revenue
+FROM orders as O JOIN dim_date as D ON O.order_date=D.date
+JOIN dim_product as P ON O.product_code=P.product_code
+
+WHERE D.year=2022 AND S.country='Germany'
+GROUP BY D.month_name,S.store_type,S.country
+ORDER BY Monthly_Revenue DESC
+LIMIT 15
